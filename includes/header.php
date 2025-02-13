770502056a5aa0c2278c8e7da7a8f0c428dacebb
Jegors Kalejs <jegors.kalejs@gmail.com>
2025-02-14 00:11:15 +0200

    Sale page, header refactor, changed file/folder structure

--- includes/header.php
+++ includes/header.php
@@ -1,53 +1,76 @@
-
-    <header>
+<header>
     <div class="header-top-part">
-            <div class="logo-img">
-                <a href="/"><img src="assets/images/logo.png" alt="Logo"></a>
-            </div>
+        <a href="index.php" class="company-name">KeyForge</a>
+        
+        <div class="search-container">
+            <i class="fas fa-search search-icon"></i>
+            <input type="text" placeholder="Spēļu, papildinājumu un citu pakalpojumu meklēšana">
+        </div>
 
-            <div>
-                <input type="text" placeholder="Meklēšana">
-            </div>
-
+        <div class="header-actions">
+    
             <div class="icons-header">
+                <div class="icons-item">
+                    <i class="fas fa-euro-sign"></i>
+                </div>
+                <div class="icons-item">
+                    <i class="fas fa-heart"></i>
+
+                </div>
                 <div class="icons-item">
                     <i class="fas fa-shopping-cart"></i>
                 </div>
-                <div class="icons-item">
-                    <i class="fa-solid fa-user"></i>
-                </div>
-                <div class="icons-item">
-                    <i class="fa-solid fa-euro-sign"></i>
-                </div>
-            </div>
-            
-        </div>
-        
-    <nav>
-        <a href="">Mājaslapa</a>
-        <div class="menu-container">
-            <a href="javascript:void(0)" class="menu-trigger">Kategorijas</a>
-            <div class="menu-content">
-                <div class="menu-section">
-                    <h3>Platformas</h3>
-                    <a href="">Steam</a>
-                    <a href="">Origin</a>
-                    <a href="">Ubisoft Connect</a>
-                </div>
-                <div class="menu-section">
-                    <h3>Žanri</h3>
-                    <a href="">Darbības spēles</a>
-                    <a href="">Sporta spēles</a>
-                    <a href="">Stratēģijas</a>
-                    <a href="">RPG</a>
-                    <a href="">Simulatori</a>
-                    <a href="">Šaušanas spēles</a>
+                <div class="user-section">
+                    <a href="login.php">Log in</a>
+                    <span>|</span>
+                    <a href="register.php">Register</a>
                 </div>
             </div>
         </div>
-        <a href="">Atlaides</a>
-        <a href="">KF-Helper</a>
-        <a href="">FAQ</a>
+    </div>
+</header>
+
+
+
+
+
+    <nav class="main-nav">
+        <div class="nav-container">
+            <a href="index.php" class="nav-link"><i class="fas fa-home"></i> Mājaslapa</a>
+            <div class="menu-container">
+                <a href="javascript:void(0)" class="menu-trigger nav-link">
+                    <i class="fas fa-gamepad"></i> Kategorijas
+                    <i class="fas fa-chevron-down"></i>
+                </a>
+                <div class="menu-content">
+                    <div class="menu-section">
+                        <h3><i class="fas fa-desktop"></i> Platformas</h3>
+                        <a href="">Steam</a>
+                        <a href="">Origin</a>
+                        <a href="">Ubisoft Connect</a>
+                        <a href="">Epic Games</a>
+                        <a href="">Battle.net</a>
+                        <a href="">EA Play</a>
+                        <a href="">Xbox</a>
+                        <a href="">PlayStation</a>
+                        
+
+                    </div>
+                    <div class="menu-section">
+
+                        <h3><i class="fas fa-tags"></i> Žanri</h3>
+                        <a href="">Sporta spēles</a>
+                        <a href="">Stratēģijas</a>
+                        <a href="">RPG</a>
+                        <a href="">Simulatori</a>
+                        <a href="">Šaušanas spēles</a>
+                    </div>
+                </div>
+            </div>
+            <a href="sale.php" class="nav-link"><i class="fas fa-percentage"></i> Atlaides</a>
+            <a href="" class="nav-link"><i class="fas fa-robot"></i> KF-Helper</a>
+            <a href="faq.php" class="nav-link"><i class="fas fa-question-circle"></i> BUJ</a>
+        </div>
     </nav>
-    </header>
+   
     <main> 
